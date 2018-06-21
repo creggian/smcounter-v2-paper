384baf65523f5fce175bee89f41ca8a0bbb06dd9
creggian <claudio.reggiani@ulb.ac.be>
2018-06-21 15:11:58 +0200

    Set workdir in R script to the script folder

--- background_error_profile/background_error_rate_modeling.R
+++ background_error_profile/background_error_rate_modeling.R
@@ -3,7 +3,22 @@
 # Chang Xu, 13NOV2017
 
 rm(list=ls())
-setwd("/u/creggian/programmi/smcounter-v2-paper/background_error_profile")
+
+thisFile <- function() {
+    cmdArgs <- commandArgs(trailingOnly = FALSE)
+    needle <- "--file="
+    match <- grep(needle, cmdArgs)
+    if (length(match) > 0) {
+        # Rscript
+        return(normalizePath(sub(needle, "", cmdArgs[match])))
+    } else {
+        # 'source'd via R console
+        return(normalizePath(sys.frames()[[1]]$ofile))
+    }
+}
+script.dir <- dirname(thisFile())
+setwd(script.dir)
+
 library(fitdistrplus)
 library(Hmisc)
 library(gPdtest)
